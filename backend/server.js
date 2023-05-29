3266db7eaabe0ce226f77d6f9c4b899cd86ce809
Anastasiia Sanina <saninaanstasia97@gmail.com>
2023-05-29 17:45:01 +0300

    Add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 8000;
 const entityRouter = require('./entityRouter');
@@ -38,6 +39,13 @@ app.use(express.json());
 app.use('/', entityRouter);
 app.use('/books', booksRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+mongoose.connect('mongodb+srv://admin:LG5guwXrMRNDqBkl@cluster0.zfpz2wv.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`)
+    });
+    console.log('Connected!');
+  })
+  .catch((error) => {
+    console.log(error)
+  });
